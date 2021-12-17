cafee149012c792b8281829a2ac00398bcfa8c97
greg-chuchro <gchuchro@acm.org>
2021-12-17 21:33:58 +0900

    #13 default clients

--- CalqTest/test-calq-client/TestCalqClient/Program.cs
+++ CalqTest/test-calq-client/TestCalqClient/Program.cs
@@ -1,34 +1,38 @@
 ﻿using System;
+using static System.Console;
 
 namespace TestCalqClient {
     class Program {
-        static void Main(string[] args) {
-            var client = TestCalqServer.HelloWorldServiceClient.Client.Instance;
-            client.Get(client.service); // get everything
+        static void Main() {
+            var service = TestCalqServer.HelloWorldServiceClient.Client.DefaultInstance.Service;
+            service.Get(); // get everything
 
-            var item = client.service.item;
-            var collection = client.service.collection;
+            var item = service.item;
+            var collection = service.collection;
 
-            Console.WriteLine(item.field); // foo
+            WriteLine(item.field); // foo
             foreach (var element in collection) {
-                Console.WriteLine(element.field);
+                WriteLine(element.field);
                 // foo 1
                 // foo 2
             }
-            
-            item.field = "foo 3"; // change locally
-            client.Put(item); // update server
-            item.field = "foo local"; // change locally
-            Console.WriteLine(item.field); // foo local
-            client.Get(item); // overwrite local changes
-            Console.WriteLine(item.field); // foo 3
+            WriteLine(item.Property.NestedProperty); // foo
 
-            Console.WriteLine(item.Property.NestedProperty); // foo nested
-            item.Property = new() { NestedProperty = "foo changed" };
-            Console.WriteLine(item.Property.NestedProperty); // foo changed
-            client.Put(item.Property); // update server
-            client.Get(item.Property); // confirm that the value has changed on the server
-            Console.WriteLine(item.Property.NestedProperty); // foo changed
+            item.field = "foo x"; // change locally
+            WriteLine(item.field); // foo x
+            item.Put(); // update server
+            item.field = "foo y"; // change locally
+            WriteLine(item.field); // foo y
+            item.Get(); // overwrite local changes
+            WriteLine(item.field); // foo x
+
+            item.Property = new() { NestedProperty = "foo x" }; // change locally
+            WriteLine(item.Property.NestedProperty); // foo x
+            item.Property.Put(); // update server
+            item.Property.NestedProperty = "foo y"; // change locally
+            WriteLine(item.Property.NestedProperty); // foo y
+            item.Property.Get(); // overwrite local changes
+            WriteLine(item.Property.NestedProperty); // foo x
         }
     }
 }
--- CalqTest/test-calq-server/TestCalqServer/HelloWorldService.cs
+++ CalqTest/test-calq-server/TestCalqServer/HelloWorldService.cs
@@ -5,7 +5,7 @@ namespace TestCalqServer {
     public class NestedResource {
         public string NestedProperty { get; set; }
         public NestedResource() {
-            NestedProperty = "foo nested";
+            NestedProperty = "foo";
         }
     }
     public class Resource {
@@ -16,8 +16,8 @@ namespace TestCalqServer {
     class HelloWorldService {
         public Resource item = new();
         public List<Resource> collection = new() {
-            new Resource { field = "foo 1" },
-            new Resource { field = "foo 2" }
+            new() { field = "foo 1" },
+            new() { field = "foo 2" }
         };
 
         static void Main() {
